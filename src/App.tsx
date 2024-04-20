106b4bee92c5c10157d854ca13f933ac64eb6e9a
shivxsh <sreeshiveshpsg@gmail.com>
2024-04-20 17:09:36 +0530

    Built responsive layout using Chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Grid, GridItem, Show } from "@chakra-ui/react"
+
 
 function App() {
-  return <Button colorScheme='blue'>Button</Button>
+  // return <Grid templateAreas={` "nav nav" "aside main" `}>
+  return <Grid templateAreas={{
+    base: ` "nav" "main" `,  //layout for mobile
+    lg: `"nav nav" "aside main"`  //layout for screens larger than 1024px
+
+    //There are other shorthands for screen sizes as well:
+    //sm, xl, md, ....
+    }}>
+
+    <GridItem area="nav" bg='red'>Nav</GridItem>
+
+    {/*<Show> is used for displaying the layout for the specified screen size (above = "")*/}
+    <Show above="lg">
+      <GridItem area="aside" bg='yellow'>Aside</GridItem>
+    </Show>
+    <GridItem area="main" bg='blue'>Main</GridItem>
+  </Grid>
 }
 
 export default App
