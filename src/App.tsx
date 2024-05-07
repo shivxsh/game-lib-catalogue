4806ce5c2effa2d36255c5f7d060f8fc7f3b769c
shivxsh <sreeshiveshpsg@gmail.com>
2024-05-07 19:11:44 +0530

    Built platform selector

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,11 @@ import GameGrid from "./components/GameGrid"
 import GenreList from "./components/GenreList"
 import { useState } from "react";
 import { Genre } from "./hooks/useGenres";
+import PlatformSelector from "./components/PlatformSelector";
 
 
 function App() {
-  const [selectedGenre, setSelectedGenre ]= useState<Genre | null>(null);
+  const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
 
 
   // return <Grid templateAreas={` "nav nav" "aside main" `}>
@@ -17,25 +18,26 @@ function App() {
 
     //There are other shorthands for screen sizes as well:
     //sm, xl, md, ....
-    }}
+  }}
     templateColumns={{
       base: '1fr',  //1 fraction: takes up all the space
       lg: '200px 1fr'
     }}>
 
     {/* Specifying 'area' will let us know, which Grid template it corresponds to */}
-    <GridItem area="nav">  
+    <GridItem area="nav">
       <NavBar />
     </GridItem>
 
     {/*<Show> is used for displaying the layout for the specified screen size (above = "")*/}
     <Show above="lg">
-      <GridItem area="aside" paddingX={5}> 
+      <GridItem area="aside" paddingX={5}>
         <GenreList onSelectGenre={(genre) => setSelectedGenre(genre)} selectedGenre={selectedGenre} />
       </GridItem>
     </Show>
     <GridItem area="main">
-      <GameGrid selectedGenre={selectedGenre}/>
+      <PlatformSelector />
+      <GameGrid selectedGenre={selectedGenre} />
     </GridItem>
   </Grid>
 }
