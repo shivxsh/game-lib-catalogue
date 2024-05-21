37eb2fe76db577aab9bca0deeb80c98344680162
shivxsh <sreeshiveshpsg@gmail.com>
2024-05-21 13:26:13 +0530

    Customizing the theme

--- src/components/theme.ts
+++ src/components/theme.ts
@@ -6,10 +6,26 @@ import { extendTheme, ThemeConfig } from '@chakra-ui/react'
 // 2. Add your color mode config
 const config: ThemeConfig = {
   initialColorMode: 'dark',
-//   useSystemColorMode: false,
+  //   useSystemColorMode: false,
 }
 
 // 3. extend the theme
-const theme = extendTheme({ config })
+const theme = extendTheme({
+  config,
+  colors: {
+    gray: {
+      50: '#F9F9F9',
+      100: '#EDEDED',
+      200: '#D3D3D3',
+      300: '#B3B3B3',
+      400: '#A0A0A0',
+      500: '#898989',
+      600: '#6C6C6C',
+      700: '#202020',
+      800: '#121212',
+      900: '#111',
+    }
+  }
+})
 
 export default theme
