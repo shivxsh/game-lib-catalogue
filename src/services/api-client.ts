70286ce59fce2c5d4967d1560b12fc2a6ec30508
shivxsh <sreeshiveshpsg@gmail.com>
2024-05-21 15:08:22 +0530

    removed API key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
-        key: '5c6955540bbc436c8fab63a9fbb1dc68'
+        key: 'Your-API-KEY'
     }
 })
