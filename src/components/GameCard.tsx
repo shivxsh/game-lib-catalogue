b938ef402f6292c381368012b7ed5e618efb765b
shivxsh <sreeshiveshpsg@gmail.com>
2024-05-21 13:06:27 +0530

    Cleaned up game cards 🧹

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -5,26 +5,26 @@ import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
 
 
-interface Props{
-    game: Game;
+interface Props {
+  game: Game;
 }
 
-const GameCard = ({game} : Props) => {
+const GameCard = ({ game }: Props) => {
 
   return (
     <Card>
-        <Image src={getCroppedImageUrl(game.background_image)}/>
-        <CardBody>
-            <Heading fontSize='2xl'>{game.name}</Heading>
-            {/* The below implementation is also correct, but it is ugly */}
-            {/* {game.parent_platforms.map( platform => <Text>{platform.platform.name}</Text>)} */}
+      <Image src={getCroppedImageUrl(game.background_image)} />
+      <CardBody>
+        {/* The below implementation is also correct, but it is ugly */}
+        {/* {game.parent_platforms.map( platform => <Text>{platform.platform.name}</Text>)} */}
 
-            {/* The below is implemented by destructuring the platform parameter */}
-            <HStack justify='space-between'>
-              <PlatformIconList platforms={game.parent_platforms.map( p => p.platform)}/>
-              <CriticScore score={game.metacritic} />
-            </HStack>
-        </CardBody>
+        {/* The below is implemented by destructuring the platform parameter */}
+        <HStack marginBottom={3} justify='space-between'>
+          <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)} />
+          <CriticScore score={game.metacritic} />
+        </HStack>
+        <Heading fontSize='2xl'>{game.name}</Heading>
+      </CardBody>
     </Card>
   )
 }
